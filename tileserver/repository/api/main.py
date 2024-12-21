2631fd91f30f470cb217f3b3bc351ece1e9dd798
Stephen Gadd <stephen@docuracy.co.uk>
2024-12-21 11:39:35 +0000

    Doc Actions

--- tileserver/repository/api/main.py
+++ tileserver/repository/api/main.py
@@ -17,3 +17,7 @@ async def get_tileserver_status():
         response = await client.get(url)
         response.raise_for_status()  # Raises an exception for 4xx/5xx responses
         return TilesMetadata(tiles=response.json())
+
+# TODO: Create endpoint to fetch mapdata from Django API and send as Job to Tippecanoe.
+
+# TODO: Create endpoints to replicate the functionality of the existing `utils`.
