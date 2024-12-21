0ef43a7a903a61368ff632b3a1977ef36bfcdd58
Stephen Gadd <stephen@docuracy.co.uk>
2024-12-21 10:35:45 +0000

    Tileserver API reconfiguration

--- tileserver/repository/api/main.py
+++ tileserver/repository/api/main.py
@@ -1,21 +1,6 @@
-import logging
 from fastapi import FastAPI
 from pydantic import BaseModel
 import httpx
-
-# Set up the root logger with a custom format
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-)
-
-# Set up Uvicorn's logger to follow the same format
-uvicorn_logger = logging.getLogger("uvicorn")
-uvicorn_logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-console_handler = logging.StreamHandler()
-console_handler.setFormatter(formatter)
-uvicorn_logger.addHandler(console_handler)
 
 # FastAPI app instance
 app = FastAPI()
@@ -28,11 +13,7 @@ class TilesMetadata(BaseModel):
 @app.get("/whg", response_model=TilesMetadata)
 async def get_tileserver_status():
     url = "http://tileserver-gl:8080/styles/whg-enhanced/style.json"
-    # Log the request
-    logging.debug(f"Requesting URL: {url}")
     async with httpx.AsyncClient() as client:
         response = await client.get(url)
         response.raise_for_status()  # Raises an exception for 4xx/5xx responses
-        # Log the response
-        logging.debug(f"Response received: {response.status_code}")
         return TilesMetadata(tiles=response.json())
