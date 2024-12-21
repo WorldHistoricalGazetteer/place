7c98ec2315bf42dd4f34bcb3b47d9a60bcc38d13
Stephen Gadd <stephen@docuracy.co.uk>
2024-12-21 10:20:53 +0000

    Tileserver API reconfiguration

--- tileserver/repository/api/main.py
+++ tileserver/repository/api/main.py
@@ -1,7 +1,16 @@
+import logging
 from fastapi import FastAPI
 from pydantic import BaseModel
 import httpx
-import logging
+
+# Configure the logger
+logger = logging.getLogger("uvicorn.error")
+logger.setLevel(logging.DEBUG)  # You can adjust the log level here
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+console_handler = logging.StreamHandler()
+
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
 
 app = FastAPI()
 
@@ -9,22 +18,13 @@ app = FastAPI()
 class TilesMetadata(BaseModel):
     tiles: dict
 
-# Enable logging
-logging.basicConfig(level=logging.DEBUG)
-
 # Endpoint to fetch data from Tileserver-GL
-@app.get("/whg", response_model=TilesMetadata)
+@app.get("/whg-enhanced", response_model=TilesMetadata)
 async def get_tileserver_status():
     url = "http://tileserver-gl:8080/styles/whg-enhanced/style.json"
-    logging.debug(f"Requesting URL: {url}")
-    try:
-        async with httpx.AsyncClient() as client:
-            response = await client.get(url)
-            response.raise_for_status()  # Raises an exception for 4xx/5xx responses
-            return TilesMetadata(tiles=response.json())
-    except httpx.RequestError as e:
-        logging.error(f"RequestError fetching data from tileserver-gl: {e}")
-        return {"error": f"RequestError fetching data from tileserver-gl: {str(e)}"}
-    except httpx.HTTPStatusError as e:
-        logging.error(f"HTTPStatusError fetching data from tileserver-gl: {e}")
-        return {"error": f"HTTPStatusError fetching data from tileserver-gl: {str(e)}"}
+    logger.debug(f"Requesting URL: {url}")  # Custom log entry
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+        response.raise_for_status()  # Raises an exception for 4xx/5xx responses
+        logger.debug(f"Response received: {response.status_code}")
+        return TilesMetadata(tiles=response.json())
