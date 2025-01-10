24964a788a96f2c995186c76d8715b18e9c61467
Stephen Gadd <stephen@docuracy.co.uk>
2025-01-10 13:56:35 +0000

    Vespa Ingestor API

--- vespa/repository/api/ingestion/processor.py
+++ vespa/repository/api/ingestion/processor.py
@@ -3,15 +3,37 @@ import asyncio
 import json
 import logging
 import tempfile
-from typing import Dict, Any
+from typing import Dict, Any, List
+
+from httpx import AsyncClient
 
 from .config import REMOTE_DATASET_CONFIGS
 from .streamer import StreamFetcher
 from .transformers import DocTransformer
+from ..config import host_mapping, batch_feed_size, namespace
 from ..feed.processor import process_documents, feed_progress
 from ..utils import log_message
 
 logger = logging.getLogger(__name__)
+
+
+async def send_batch(feed_service_url: str, batch: List[Dict[str, Any]], logger: logging.Logger, task_id: str) -> None:
+    """
+    Send a batch of documents to the Vespa feed service.
+
+    :param feed_service_url: The URL of the Vespa feed service
+    :param batch: A list of documents to send
+    :param logger: The logger for logging
+    :param task_id: The task ID for progress tracking
+    """
+    async with AsyncClient() as client:
+        try:
+            response = await client.post(feed_service_url, json=batch)
+            response.raise_for_status()
+            log_message(logger.info, feed_progress, task_id, "success",
+                        f"Successfully fed a batch of {len(batch)} documents")
+        except Exception as e:
+            log_message(logger.error, feed_progress, task_id, "error", f"Error sending batch: {e}")
 
 
 def process_dataset(dataset_name: str, task_id: str, limit: int = None) -> Dict[str, Any]:
@@ -22,6 +44,9 @@ def process_dataset(dataset_name: str, task_id: str, limit: int = None) -> Dict[
     :param task_id: The task ID for tracking the progress of the ingestion
     :param limit: The maximum number of items to ingest
     """
+
+    feed_service_url = host_mapping['feed']
+
     # Get the configuration for the dataset
     dataset_config = next((config for config in REMOTE_DATASET_CONFIGS if config['dataset_name'] == dataset_name), None)
 
@@ -30,13 +55,10 @@ def process_dataset(dataset_name: str, task_id: str, limit: int = None) -> Dict[
             logger.info, feed_progress, task_id, "error",
             f"Dataset configuration not found for dataset: {dataset_name}"
         )
-    else:
-        log_message(
-            logger.info, feed_progress, task_id, "processing",
-            f"Processing dataset: {dataset_name}"
-        )
-
-    # TODO: Either remove existing data from Vespa or update
+    log_message(
+        logger.info, feed_progress, task_id, "processing",
+        f"Processing dataset: {dataset_name}"
+    )
 
     try:
         # Process each file in the dataset configuration
@@ -45,36 +67,28 @@ def process_dataset(dataset_name: str, task_id: str, limit: int = None) -> Dict[
             # Use StreamFetcher to get the stream of data from the file URL
             stream_fetcher = StreamFetcher(file_config)
             documents = stream_fetcher.get_items()
-            with tempfile.NamedTemporaryFile(delete=False) as document_file:
-                temp_file_path = document_file.name
-                log_message(
-                    logger.info, feed_progress, task_id, "processing",
-                    f"Processing dataset: {dataset_name} ({i + 1}/{len(dataset_config['files'])})"
-                )
-                for count, document in enumerate(documents):
-                    if limit is not None and count >= limit:
-                        break
-                    # log_message(
-                    #     logger.info, feed_progress, task_id, "processing",
-                    #     f"Processing document {count + 1}: {str(document)[:3000]}..."
-                    # )
-                    transformed_document, toponyms = DocTransformer.transform(document, dataset_name,
-                                                                              transformer_index=i)
-                    document_file.write(
-                        json.dumps(transformed_document).encode('utf-8'))  # Write each transformed document to the file
-                document_file.close()
 
-            # Process the file
-            try:
-                log_message(
-                    logger.info, feed_progress, task_id, "processing",
-                    f"Sending documents to Vespa: {dataset_name} ({i + 1}/{len(dataset_config['files'])})"
-                )
-                asyncio.run(process_documents(dataset_config['vespa_schema'], temp_file_path, logger, task_id))
-            finally:
-                # Ensure the tempfile is deleted even if processing fails # TODO !
-                # os.remove(temp_file_path)
-                pass
+            batch = []
+            batch_count = 0
+
+            for count, document in enumerate(documents):
+                if limit is not None and count >= limit:
+                    break
+
+                transformed_document, toponyms = DocTransformer.transform(document, dataset_name, transformer_index=i)
+                batch.append(transformed_document)
+
+                if len(batch) >= batch_feed_size:
+                    batch_count += 1
+                    log_message(logger.info, feed_progress, task_id, "processing", f"Sending batch {batch_count}")
+                    asyncio.run(send_batch(feed_service_url, batch, logger, task_id))
+                    batch.clear()
+
+            # Send any remaining documents in the last batch
+            if batch:
+                batch_count += 1
+                log_message(logger.info, feed_progress, task_id, "processing", f"Sending final batch {batch_count}")
+                asyncio.run(send_batch(feed_service_url, batch, logger, task_id))
 
         return log_message(
             logger.info, feed_progress, task_id, "success",
