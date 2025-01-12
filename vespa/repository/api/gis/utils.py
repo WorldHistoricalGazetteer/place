61a281b7f96b6c83be65311a99d5f076a0aa18fe
Stephen Gadd <stephen@docuracy.co.uk>
2025-01-12 07:48:12 +0000

    Vespa Query API

--- vespa/repository/api/gis/utils.py
+++ vespa/repository/api/gis/utils.py
@@ -105,9 +105,10 @@ def box_intersect(test_box, schema_name, schema_fields="*", schema_box="bbox"):
                 "yql": f"""
                         select {schema_fields} 
                         from sources {schema_name} 
-                        where bbox_sw_lat = 10.140054
+                        where bbox_sw_lat <= 10.140054
                         """
             }
+            logger.info(f"Query: {query}")
 
             # if test_box["sw"]["lng"] > test_box["ne"]["lng"]:
             #     query = {
