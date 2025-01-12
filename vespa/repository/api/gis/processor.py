8aa675eb6144e55d8acb7cf2e40cdfcad8e2cb0d
Stephen Gadd <stephen@docuracy.co.uk>
2025-01-12 14:28:13 +0000

    Vespa GeometryProcessor

--- vespa/repository/api/gis/processor.py
+++ vespa/repository/api/gis/processor.py
@@ -85,11 +85,11 @@ class GeometryProcessor:
 
         result = {
             **({"area": area} if area else {}),
-            # **bbox_codes,
-            **({"convex_hull": to_geojson(convex_hull)} if convex_hull else {}),
+            **bbox_codes,
+            # **({"convex_hull": to_geojson(convex_hull)} if convex_hull else {}),
             **({"geometry": json.dumps(float_geometry)} if float_geometry else {}),
             **({"length": length} if length else {}),
-            **({"representative_point": to_geojson(representative_point) if representative_point else {}}),
+            # **({"representative_point": to_geojson(representative_point) if representative_point else {}}),
         }
 
         logging.info(f"Processed geometry: {result}")
