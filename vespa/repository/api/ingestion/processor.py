c1daa055a9a9341c5f9deabb0790e90f37f28b0b
Stephen Gadd <stephen@docuracy.co.uk>
2025-01-16 09:26:53 +0000

    GeometryProcessor multi-temporal validator; document namespacing; ISO3166 upgrade

--- vespa/repository/api/ingestion/processor.py
+++ vespa/repository/api/ingestion/processor.py
@@ -108,7 +108,9 @@ async def process_document(document, dataset_config, transformer_index, sync_app
         if success and toponyms:
             toponym_responses = await asyncio.gather(*[
                 asyncio.get_event_loop().run_in_executor(
-                    executor, feed_document, sync_app, 'toponym', None, toponym['record_id'], toponym
+                    executor, feed_document, sync_app, 'toponym', None, toponym['record_id'], {
+                        key: value for key, value in toponym.items() if key != 'record_id'
+                    } # Remove record_id from toponym document
                 )
                 for toponym in toponyms
             ])
