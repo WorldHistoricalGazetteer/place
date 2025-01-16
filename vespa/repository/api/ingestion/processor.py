71b13ab9092f2ad0ba6c007e0061573641e88df1
Stephen Gadd <stephen@docuracy.co.uk>
2025-01-16 21:51:25 +0000

    GeometryProcessor multi-temporal validator; document namespacing; ISO3166 upgrade

--- vespa/repository/api/ingestion/processor.py
+++ vespa/repository/api/ingestion/processor.py
@@ -291,7 +291,7 @@ def delete_all_docs(sync_app, dataset_config):
             namespace=namespace,
             schema=schema,
             wantedDocumentCount=100,
-            fieldSet="[all]"
+            fieldSet="[toponyms]"
         ):
             for response in slice:
                 for document in response.documents:
