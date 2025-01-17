aa6ce4986ff6d73821baecb614cd1ea3a7792799
Stephen Gadd <stephen@docuracy.co.uk>
2025-01-17 10:25:36 +0000

    ISO3166 testing

--- vespa/repository/api/gis/intersections.py
+++ vespa/repository/api/gis/intersections.py
@@ -116,6 +116,7 @@ class BoxIntersect:
                     namespace=self.namespace,
                     schema=self.schema,
                 ).json
+                logger.info(f"Vespa response: {response}")
                 if "error" in response:
                     raise ValueError(f"Error during Vespa query: {response['error']}")
                 return [child.get("fields", {}) for child in response.get("root", {}).get("children", [])]
