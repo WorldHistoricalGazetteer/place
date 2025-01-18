e48ed59dca1207bc9811f80045fc94e749a657d0
Stephen Gadd <stephen@docuracy.co.uk>
2025-01-18 18:02:42 +0000

    Pleiades ingestor

--- vespa/repository/api/ingestion/processor.py
+++ vespa/repository/api/ingestion/processor.py
@@ -63,10 +63,10 @@ def feed_document(sync_app, namespace, schema, transformed_document, task_id):
             # Check if toponym already exists
             with VespaClient.sync_context("feed") as sync_app:
                 bcp47_fields = ["language", "script", "region", "variant"]
-                yql = f'select documentid, places from toponym where name matches "^{transformed_document['fields']['name']}$" '
+                yql = f'select documentid, places from toponym where name matches "^{transformed_document["fields"]["name"]}$" '
                 for field in bcp47_fields:
                     if transformed_document.get("fields", {}).get(f"bcp47_{field}"):
-                        yql += f'and bcp47_{field} matches "^{transformed_document['fields'][f'bcp47_{field}']}$" '
+                        yql += f'and bcp47_{field} matches "^{transformed_document["fields"][f"bcp47_{field}"]}$" '
                 yql += 'limit 1'
                 # logger.info(f"Checking if toponym exists: {yql}")
                 existing_response = sync_app.query({'yql': yql}).json
