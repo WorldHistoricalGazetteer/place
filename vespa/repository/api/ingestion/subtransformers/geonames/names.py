fe46d3cf741b4092faf8f6810e8f78a2fc1a4dcc
Stephen Gadd <stephen@docuracy.co.uk>
2025-01-20 12:18:26 +0000

    GeoNames Ingestor

--- vespa/repository/api/ingestion/subtransformers/geonames/names.py
+++ vespa/repository/api/ingestion/subtransformers/geonames/names.py
@@ -27,7 +27,7 @@ class NamesProcessor:
         :return: A dictionary with 'names' and 'toponyms' arrays.
         """
 
-        logger.info(f"Processing names for document {self.document_id}: {len(self.name)} names found. {self.name}")
+        # logger.info(f"Processing names for document {self.document_id}: {len(self.name)} names found. {self.name}")
 
         alternateName = self.name.get("alternate_name")
         if not alternateName:
