119d99d75216c87e150c9b82c287614816674d0a
Stephen Gadd <stephen@docuracy.co.uk>
2025-01-20 16:32:46 +0000

    GeoNames Ingestor

--- vespa/repository/api/ingestion/processor.py
+++ vespa/repository/api/ingestion/processor.py
@@ -67,6 +67,7 @@ def feed_document(sync_app, namespace, schema, transformed_document, task_id, co
             with VespaClient.sync_context("feed") as sync_app:
                 bcp47_fields = ["language", "script", "region", "variant"]
                 yql = f'select documentid, places from toponym where name matches "^{escape_yql(transformed_document["fields"]["name"])}$" '
+                # yql = f'select documentid, places from toponym where name_strict contains "{escape_yql(transformed_document["fields"]["name"])}" '
                 for field in bcp47_fields:
                     if transformed_document.get("fields", {}).get(f"bcp47_{field}"):
                         yql += f'and bcp47_{field} matches "^{transformed_document["fields"][f"bcp47_{field}"]}$" '
