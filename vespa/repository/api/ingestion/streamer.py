63ecdf1845c527f324206acb44a0f1dc64459a92
Stephen Gadd <stephen@docuracy.co.uk>
2025-01-20 11:24:51 +0000

    GeoNames Ingestor

--- vespa/repository/api/ingestion/streamer.py
+++ vespa/repository/api/ingestion/streamer.py
@@ -138,7 +138,7 @@ class StreamFetcher:
 
         if format_type in ['json', 'geojson']:
             return self._parse_json_stream(stream)
-        elif format_type == 'csv':
+        elif format_type in ['csv', 'tsv', 'txt']:
             return self._parse_csv_stream(stream)
         elif format_type == 'xml':
             return self._parse_xml_stream(stream)
