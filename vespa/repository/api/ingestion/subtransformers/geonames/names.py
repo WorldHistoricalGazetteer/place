30a2ee08713cea79187aa2fed95b1d8df9df972f
Stephen Gadd <stephen@docuracy.co.uk>
2025-01-20 11:43:04 +0000

    GeoNames Ingestor

--- vespa/repository/api/ingestion/subtransformers/geonames/names.py
+++ vespa/repository/api/ingestion/subtransformers/geonames/names.py
@@ -7,14 +7,14 @@ logger = logging.getLogger(__name__)
 
 
 class NamesProcessor:
-    def __init__(self, document_id: str, names: List[Dict[str, Any]]):
+    def __init__(self, document_id: str, name: Dict[str, Any]):
         """
         :param document_id: The unique ID of the document (place).
         :param names: List of name dictionaries containing identifiers and 'alternate_name', 'isolanguage',
             'isPreferredName', 'from', and 'to'.
         """
         self.document_id = document_id
-        self.names = names
+        self.name = name
         self.output = {
             'names': [],
             'toponyms': [],
@@ -29,34 +29,32 @@ class NamesProcessor:
 
         logger.info(f"Processing names for document {self.document_id}: {len(self.names)} names found. {self.names}")
 
-        for name in self.names:
+        alternateName = self.name.get("alternate_name")
+        if not alternateName:
+            return self.output
 
-            alternateName = name.get("alternate_name")
-            if not alternateName:
-                continue
+        isolanguage = self.name.get('isolanguage')
+        if isolanguage and isolanguage in ["post", "iata", "icao", "faac", "abbr", "link", "wkdt"]:
+            # Skip non-language codes, move on to next name
+            return self.output
 
-            isolanguage = name.get('isolanguage')
-            if isolanguage and isolanguage in ["post", "iata", "icao", "faac", "abbr", "link", "wkdt"]:
-                # Skip non-language codes, move on to next name
-                continue
+        years = {
+            **({'year_start': year_start} if (year_start := self.name.get('from')) else {}),
+            **({'year_end': year_end} if (year_end := self.name.get('to')) else {}),
+        }
 
-            years = {
-                **({'year_start': year_start} if (year_start := name.get('from')) else {}),
-                **({'year_end': year_end} if (year_end := name.get('to')) else {}),
+        self.output['names'].append({
+            'toponym_id': (toponym_id := self.name.get("alternateNameId", get_uuid())),
+            **years,
+            **({'is_preferred': is_preferred} if (is_preferred := self.name.get('isPreferredName')) else {}),
+        })
+        self.output['toponyms'].append({
+            'document_id': toponym_id,
+            'fields': {
+                'name': alternateName,
+                'places': [self.document_id],
+                **({'bcp47_language': isolanguage} if isolanguage else {}),
             }
-
-            self.output['names'].append({
-                'toponym_id': (toponym_id := name.get("alternateNameId", get_uuid())),
-                **years,
-                **({'is_preferred': is_preferred} if (is_preferred := name.get('isPreferredName')) else {}),
-            })
-            self.output['toponyms'].append({
-                'document_id': toponym_id,
-                'fields': {
-                    'name': alternateName,
-                    'places': [self.document_id],
-                    **({'bcp47_language': isolanguage} if isolanguage else {}),
-                }
-            })
+        })
 
         return self.output
