0aab9c3273b81079a1686ec1214ac5e91ee2cb13
Stephen Gadd <stephen@docuracy.co.uk>
2025-01-28 10:51:14 +0000

    LOC ingestion

--- vespa/repository/api/ingestion/config.py
+++ vespa/repository/api/ingestion/config.py
@@ -143,17 +143,18 @@ REMOTE_DATASET_CONFIGS = [
                         "madsrdf:GeographicElement" in graph_item.get("@type", [])
                         for graph_item in record.get("@graph", [])
                     ) and any(
-                        ("madsrdf:hasCloseExternalAuthority" in graph_item or
-                         ("madsrdf:hasExactExternalAuthority" in graph_item and
-                          # Exclude VIAF links which are typically invalid, and essentially the same as the LOC ID
-                          isinstance(graph_item, dict) and # Allow if there is a list of more than one external authorities
-                          (
-                              # Check if madsrdf:hasExactExternalAuthority is an object
-                                  isinstance(graph_item.get("madsrdf:hasExactExternalAuthority", {}), dict) and
-                                  not graph_item["madsrdf:hasExactExternalAuthority"].get("@id", "").startswith(
-                                      "http://viaf.org/")
-                          )
-                          ))
+                        (
+                            "madsrdf:hasCloseExternalAuthority" in graph_item
+                            and (
+                                isinstance(graph_item["madsrdf:hasExactExternalAuthority"], list)
+                                or
+                                (
+                                    isinstance(graph_item["madsrdf:hasExactExternalAuthority"], dict)
+                                    and
+                                    not graph_item["madsrdf:hasExactExternalAuthority"].get("@id", "").startswith("http://viaf.org/")
+                                )
+                            )
+                        )
                         for graph_item in record.get("@graph", [])
                     )
                 ]
