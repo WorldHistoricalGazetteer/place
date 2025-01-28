587d47a6574faf0efffd19160d9f92ff29efdc6a
Stephen Gadd <stephen@docuracy.co.uk>
2025-01-28 16:49:23 +0000

    LOC ingestion

--- vespa/repository/api/ingestion/namespace.py
+++ vespa/repository/api/ingestion/namespace.py
@@ -19,41 +19,45 @@ namespaces = {
         "url": lambda curie: f"http://catalogue.bnf.fr/ark:/12148/{curie.split(':', 1)[-1]}",
         "match": r"^https?://(www\.)?catalogue\.bnf\.fr/ark:/12148/(?P<id>.*)$",
     },
-    "cantic": {
-        "url": lambda curie: f"http://cantic.bnc.cat/registres/CUCId/{curie.split(':', 1)[-1]}",
-        "match": r"^https?://(www\.)?cantic\.bnc\.cat/registres/CUCId/(?P<id>.*)$",
-    },
     "cabi": {
         "url": lambda curie: f"http://id.cabi.org/cabt/{curie.split(':', 1)[-1]}",
         "match": r"^https?://(www\.)?id\.cabi\.org/cabt/(?P<id>.*)$",
+    },
+    "cantic": {
+        "url": lambda curie: f"http://cantic.bnc.cat/registres/CUCId/{curie.split(':', 1)[-1]}",
+        "match": r"^https?://(www\.)?cantic\.bnc\.cat/registres/CUCId/(?P<id>.*)$",
     },
     "dbp": {
         "url": lambda curie: f"http://dbpedia.org/resource/{curie.split(':', 1)[-1]}",
         "match": r"^https?://(www\.)?dbpedia\.org/resource/(?P<id>.*)$",
     },
-    "eurovoc": {
-        "url": lambda curie: f"http://eurovoc.europa.eu/{curie.split(':', 1)[-1]}",
-        "match": r"^https?://(www\.)?eurovoc\.europa\.eu/(?P<id>.*)$",
+    "eionet": {
+        "url": lambda curie: f"http://eionet.europa.eu/gemet/{curie.split(':', 1)[-1]}",
+        "match": r"^https?://(www\.)?eionet\.europa\.eu/gemet/(?P<id>.*)$",
     },
     "emlo": {
         "url": lambda curie: f"http://emlo.bodleian.ox.ac.uk/profile/location/{curie.split(':', 1)[-1]}",
         "match": r"^https?://(www\.)?emlo\.bodleian\.ox\.ac\.uk/profile/location/(?P<id>.*)$",
     },
+    "eurovoc": {
+        "url": lambda curie: f"http://eurovoc.europa.eu/{curie.split(':', 1)[-1]}",
+        "match": r"^https?://(www\.)?eurovoc\.europa\.eu/(?P<id>.*)$",
+    },
     "fast": {
         "url": lambda curie: f"http://id.worldcat.org/fast/{curie.split(':', 1)[-1]}",
         "match": r"^https?://(www\.)?id\.worldcat\.org/fast/(?P<id>.*)$",
     },
+    "gesis": {
+        "url": lambda curie: f"http://lod.gesis.org/thesoz/{curie.split(':', 1)[-1]}",
+        "match": r"^https?://(www\.)?lod\.gesis\.org/thesoz/(?P<id>.*)$",
+    },
     "gn": {
         "url": lambda curie: f"http://sws.geonames.org/{curie.split(':', 1)[-1]}/",
-        "match": r"^https?://(www\.)?sws\.geonames\.org/(?P<id>.*)(?:/)$",
+        "match": r"^https?://(www\.)?sws\.geonames\.org/(?P<id>\d+)(?:/.*)?$"
     },
     "gnd": {
         "url": lambda curie: f"http://d-nb.info/gnd/{curie.split(':', 1)[-1]}",
         "match": r"^https?://(www\.)?d-nb\.info/gnd/(?P<id>.*)$",
-    },
-    "gesis": {
-        "url": lambda curie: f"http://lod.gesis.org/thesoz/{curie.split(':', 1)[-1]}",
-        "match": r"^https?://(www\.)?lod\.gesis\.org/thesoz/(?P<id>.*)$",
     },
     "idref": {
         "url": lambda curie: f"http://idref.fr/{curie.split(':', 1)[-1]}",
@@ -71,6 +75,10 @@ namespaces = {
         "url": lambda curie: f"http://imperium.ahlfeldt.se/places/{curie.split(':', 1)[-1]}",
         "match": r"^https?://(www\.)?imperium\.ahlfeldt\.se/places/(?P<id>.*)$",
     },
+    "lgd": {
+        "url": lambda curie: f"http://linkedgeodata.org/triplify/{curie.split(':', 1)[-1]}",
+        "match": r"^https?://(www\.)?linkedgeodata\.org/triplify/(?P<id>.*)$",
+    },
     "logainm": {
         "url": lambda curie: f"http://logainm.ie/{curie.split(':', 1)[-1]}",
         "match": r"^https?://(www\.)?logainm\.ie/(?P<id>.*)$",
@@ -81,7 +89,11 @@ namespaces = {
     },
     "linz": {
         "url": lambda curie: f"http://gazetteer.linz.govt.nz/place/{curie.split(':', 1)[-1]}",
-        "match": r"^https?://(www\.)?gazetteer\.linz\.govt\.nz/place//(?P<id>.*)$",
+        "match": r"^https?://(www\.)?gazetteer\.linz\.govt\.nz/place/(?P<id>.*)$",
+    },
+    "mesh": {
+        "url": lambda curie: f"http://id.nlm.nih.gov/mesh/{curie.split(':', 1)[-1]}",
+        "match": r"^https?://(www\.)?id\.nlm\.nih\.gov/mesh/(?P<id>.*)$",
     },
     "mindat": {
         "url": lambda curie: f"http://mindat.org/{curie.split(':', 1)[-1]}",
@@ -107,13 +119,25 @@ namespaces = {
         "url": lambda curie: f"http://pleiades.stoa.org/places/{curie.split(':', 1)[-1]}",
         "match": r"^https?://(www\.)?pleiades\.stoa\.org/places/(?P<id>.*)$",
     },
-    "stwf": {
+    "stw": {
         "url": lambda curie: f"http://zbw.eu/stw/descriptor/{curie.split(':', 1)[-1]}",
         "match": r"^https?://(www\.)?zbw\.eu/stw/descriptor/(?P<id>.*)$",
     },
     "tgn": {
-        "url": lambda curie: f"http://vocab.getty.edu/tgn/{curie.split(':', 1)[-1].removesuffix('-place')}",
+        "url": lambda curie: f"http://vocab.getty.edu/tgn/{curie.split(':', 1)[-1]}",
         "match": r"^https?://(www\.)?vocab\.getty\.edu/(.*/)?tgn/(?P<id>.*)(?:-place)?$",
+    },
+    "un": {
+        "url": lambda curie: f"http://metadata.un.org/thesaurus/{curie.split(':', 1)[-1]}",
+        "match": r"^https?://(www\.)?metadata\.un\.org/thesaurus/(?P<id>.*)$",
+    },
+    "unesco": {
+        "url": lambda curie: f"http://vocabularies.unesco.org/thesaurus/{curie.split(':', -1)[-1]}",
+        "match": r"^https?://(www\.)?vocabularies\.unesco\.org/thesaurus/(?P<id>.*)$",
+    },
+    "usgs": {
+        "url": lambda curie: f"http://edits.nationalmap.gov/apps/gaz-domestic/public/{curie.split(':', 1)[-1]}",
+        "match": r"^https?://(www\.)?edits\.nationalmap\.gov/apps/gaz-domestic/public/(?P<id>.*)$",
     },
     "viaf": {
         "url": lambda curie: f"http://viaf.org/viaf/{curie.split(':', 1)[-1]}",
