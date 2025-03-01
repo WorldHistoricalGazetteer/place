710a7322c9c83776bf47c06fffa32b00c49fd712
Stephen Gadd <stephen@docuracy.co.uk>
2025-03-01 18:14:38 +0000

    Vespa API

--- vespa/repository/api/ingestion/processor.py
+++ vespa/repository/api/ingestion/processor.py
@@ -109,11 +109,11 @@ class IngestionManager:
 
         for schema in schema:
             # https://pyvespa.readthedocs.io/en/latest/reference-api.html#vespa.application.Vespa.delete_all_docs
-            self.vespa_client.delete_all_docs(
+            logger.info( self.vespa_client.delete_all_docs(
                 namespace=self.dataset_config['namespace'],
                 schema=schema,
                 content_cluster_name="content"
-            )
+            ) )
             logger.info(f"Deleted {self.dataset_config['namespace']}:{schema} documents.")
 
     async def ingest_data(self):
