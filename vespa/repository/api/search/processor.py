2927d4ee9ee4c1979ff0fe32485f9886f15e8321
Stephen Gadd <stephen@docuracy.co.uk>
2025-03-10 12:46:05 +0000

    Vespa: Search API

--- vespa/repository/api/search/processor.py
+++ vespa/repository/api/search/processor.py
@@ -204,7 +204,7 @@ def _locate_by_point(sync_app, point, radius, limit, namespace):
     else:
         x, y, z = geo_to_cartesian(lat, lon)
         conditions = [
-            f'''{{targetHits: {limit}}}nearestNeighbor(cartesian, [{x}, {y}, {z}])'''
+            f'{{"targetHits": {limit}}}nearestNeighbor(cartesian, [{x}, {y}, {z}])'
         ]
 
     where_clause = " and ".join(conditions)
