decb76e203db0fdf5504aef42740e9578cc89438
Stephen Gadd <42514781+docuracy@users.noreply.github.com>
2025-07-29 10:32:11 +0200

    Ingestion to ix1 + split to parquet + transform to DuckDB

---
+++ vespa/repository/api/ingestion/test.py
@@ -0,0 +1,17 @@
+import gzip, json
+from urllib.request import urlopen
+
+url = 'https://dumps.wikimedia.org/wikidatawiki/entities/latest-all.json.gz'
+
+with urlopen(url) as r:
+    with gzip.GzipFile(fileobj=r) as f:
+        for j, ln in enumerate(f):
+            if ln == b'[\n' or ln == b']\n':
+                continue
+            if ln.endswith(b',\n'):  # all but the last element
+                obj = json.loads(ln[:-2])
+            else:
+                obj = json.loads(ln)
+
+            print(obj['id'], end=' ')
+            if j == 5: break
