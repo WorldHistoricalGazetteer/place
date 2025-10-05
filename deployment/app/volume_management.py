f1a99d92c24a471d2bdafd81f8fc2dfc6810f77b
Stephen Gadd <stephen@docuracy.co.uk>
2025-10-05 08:49:32 +0100

    Tileserver

--- deployment/app/volume_management.py
+++ deployment/app/volume_management.py
@@ -131,11 +131,6 @@ def ensure_pv_directories(volumes):
             try:
                 os.makedirs(path, mode=perms, exist_ok=True)
                 logger.info(f"Created {path} with perms {vol['perms']}")
-                """                
-                drwxr-xr-x.  2 root root  2 Oct  4 12:04 django-app
-                drwxr-xr-x.  2 root root  2 Oct  4 12:04 postgres-staging
-                drwxr-xr-x.  2 root root  2 Oct  4 12:04 vespa-ingestion
-                """
             except OSError as e:
                 if e.errno != errno.EEXIST:
                     raise
